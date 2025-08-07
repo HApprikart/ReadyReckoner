e13d33a51d20ed591fc1a9cf836244cea3f6815f
Suneel <developer@apprikart.com>
2025-08-07 14:40:09 +0530

    Updated code

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReadyReckoner",
-            url: "https://hyundai-tab-ssc.s3.ap-south-1.amazonaws.com/Kiosk+Build/ReadyReckonerIOS/070825/ReckonerKit.zip",
-            checksum: "650a2583129754971fb86b3c77c7609d1eb6312aab541d15084650ca558297bc"
+            url: "https://hyundai-tab-ssc.s3.ap-south-1.amazonaws.com/Kiosk+Build/ReadyReckonerIOS/0708252/ReckonerKit.zip",
+            checksum: "c01a586d18893c698e50b2b2254aab8a88e3422f9041a4c00e3e27b16ddf31f7"
         )
     ]
 )
