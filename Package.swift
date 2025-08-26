876ebd7ade986bf42acc721667a6282104d0385a
Suneel <developer@apprikart.com>
2025-08-26 15:45:48 +0530

    Fixed color related bugs

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ReadyReckoner",
-            url: "https://hyundai-tab-ssc.s3.ap-south-1.amazonaws.com/Kiosk+Build/ReadyReckonerIOS/0708252/ReckonerKit.zip",
-            checksum: "c01a586d18893c698e50b2b2254aab8a88e3422f9041a4c00e3e27b16ddf31f7"
+            url: "https://hyundai-tab-ssc.s3.ap-south-1.amazonaws.com/Kiosk+Build/ReadyReckonerIOS/ReckonerKit.zip",
+            checksum: "59a693dfa7aa1c82911ae873078ab2112a2d8e6154fd93d1b04675423d7537a8"
         )
     ]
 )
